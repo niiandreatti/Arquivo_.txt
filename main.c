e8152f094e80e5dcdeeeb2f6eecc85063f5684ce
Nicolas Andreatti de Santana <114240908+niiandreatti@users.noreply.github.com>
2024-09-12 20:42:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     // Cria e abre o arquivo exemplo.txt para escrita
     FILE *arquivo = fopen("exemplo.txt", "w");
+    int contador = 0;
+    char linha[100][256];
     if (arquivo == NULL) {
         printf("Não foi possível abrir o arquivo.\n");
-        return 1; // Retorna um código de erro
+        return 1; 
     }
 
-    // Escreve algumas linhas no arquivo
+
     fprintf(arquivo, "Olá, Mundo!\n");
     fprintf(arquivo, "Este é um exemplo de escrita em um arquivo .txt.\n");
 
@@ -16,21 +19,23 @@ int main() {
     fclose(arquivo);
 
     printf("Dados foram escritos com sucesso em exemplo.txt\n");
-    return 0;
     
+    arquivo = fopen("exemplo.txt", "r");
+    if (arquivo == NULL) {
+        printf("Não foi possível abrir o arquivo para leitura.\n");
+        return 1;
+    }
     // ler e mostrar a quantidade de linhas no arquivo
-    int contador = 0;
-    FILE *arquivo = fopen("exemplo.txt", "r");
-        if(arquivo == NULL){
-            return 1;
-        }else{
-            char linha[256];
-            while(fgets(linha, sizeof(linha), arquivo) != NULL){
-                printf("%s", linha);
-                contador ++;
-            }
+    while(fgets(linha[contador],256, arquivo) != NULL){
+        linha[contador][strcspn(linha[contador],"\n")] = '\0';
+        contador ++;
+    }
             
-            fclose(arquivo);
-            printf("\n a quantidade de linha no arquivo é: %d", contador);
-        }
+    fclose(arquivo);
+    printf("\n a quantidade de linha no arquivo é: %d\n", contador);
+    
+    for(int i = 0;i<contador;i++){
+        printf("%s\n", linha[i]);
+    }
+    
 }
